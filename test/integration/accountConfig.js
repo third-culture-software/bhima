72f6c2eb742ee33cde587a97ae0892639647604e
Jonathan Niles <jonathanwniles@gmail.com>
2024-12-15 15:37:23 -0600

    WIP(payroll): refactor payroll account_config
    Refactors and renames the API for payroll account configurations from
    /account_config to /payroll/account_config to prevent confusion.
    Updates comments to ensure correctness and usefulness.  Also migrates
    the API to using async/await.

--- test/integration/accountConfig.js
+++ test/integration/accountConfig.js
@@ -2,10 +2,10 @@
 
 const helpers = require('./helpers');
 
-/*
- * The /payroll/account_configuration  API
+/**
+ * The /payroll/account_config API
  *
- * This test suite implements full CRUD on the /payroll/account_configuration  API.
+ * This test suite implements full CRUD on the /payroll/account_configAPI.
  */
 describe('test/integration/accountConfig Account configuration API', () => {
   // Account Payroll Configuration we will add during this test suite.
@@ -21,16 +21,16 @@ describe('test/integration/accountConfig Account configuration API', () => {
 
   const NUM_ACCOUNTING = 1;
 
-  it('GET /ACCOUNT_CONFIG returns a list of Account Configurations ', () => {
-    return agent.get('/account_config')
+  it('GET /payroll/account_config returns a list of Account Configurations ', () => {
+    return agent.get('/payroll/account_config')
       .then((res) => {
         helpers.api.listed(res, NUM_ACCOUNTING);
       })
       .catch(helpers.handler);
   });
 
-  it('POST /ACCOUNT_CONFIG should create a new Account Configuration', () => {
-    return agent.post('/account_config')
+  it('POST /payroll/account_config should create a new Account Configuration', () => {
+    return agent.post('/payroll/account_config')
       .send(accountConfig)
       .then((res) => {
         accountConfig.id = res.body.id;
@@ -39,24 +39,24 @@ describe('test/integration/accountConfig Account configuration API', () => {
       .catch(helpers.handler);
   });
 
-  it('GET /ACCOUNT_CONFIG/:ID should not be found for unknown id', () => {
-    return agent.get('/account_config/123456789')
+  it('GET /payroll/account_config/:id should not be found for unknown id', () => {
+    return agent.get('/payroll/account_config/123456789')
       .then((res) => {
         helpers.api.errored(res, 404);
       })
       .catch(helpers.handler);
   });
 
-  it('GET /ACCOUNT_CONFIG/:ID will send back a 404 if the Account Configuration is a string', () => {
-    return agent.get('/account_config/str')
+  it('GET /payroll/account_config/:id will send back a 404 if the Account Configuration is a string', () => {
+    return agent.get('/payroll/account_config/str')
       .then((res) => {
         helpers.api.errored(res, 404);
       })
       .catch(helpers.handler);
   });
 
-  it('PUT /ACCOUNT_CONFIG  should update an existing Account Configuration', () => {
-    return agent.put('/account_config/'.concat(accountConfig.id))
+  it('PUT /payroll/account_config  should update an existing Account Configuration', () => {
+    return agent.put('/payroll/account_config/'.concat(accountConfig.id))
       .send(accountConfigUpdate)
       .then((res) => {
         expect(res).to.have.status(200);
@@ -65,33 +65,33 @@ describe('test/integration/accountConfig Account configuration API', () => {
       .catch(helpers.handler);
   });
 
-  it('GET /ACCOUNT_CONFIG/:ID returns a single Account Configuration', () => {
-    return agent.get('/account_config/'.concat(accountConfig.id))
+  it('GET /payroll/account_config/:id returns a single Account Configuration', () => {
+    return agent.get('/payroll/account_config/'.concat(accountConfig.id))
       .then((res) => {
         expect(res).to.have.status(200);
       })
       .catch(helpers.handler);
   });
 
-  it('DELETE /ACCOUNT_CONFIG/:ID will send back a 404 if the Account Configuration does not exist', () => {
-    return agent.delete('/account_config/123456789')
+  it('DELETE /payroll/account_config/:id will send back a 404 if the Account Configuration does not exist', () => {
+    return agent.delete('/payroll/account_config/123456789')
       .then((res) => {
         helpers.api.errored(res, 404);
       })
       .catch(helpers.handler);
   });
 
-  it('DELETE /ACCOUNT_CONFIG/:ID will send back a 404 if the Account Configuration is a string', () => {
-    return agent.delete('/account_config/str')
+  it('DELETE /payroll/account_config/:id will send back a 404 if the Account Configuration is a string', () => {
+    return agent.delete('/payroll/account_config/str')
       .then((res) => {
         helpers.api.errored(res, 404);
       })
       .catch(helpers.handler);
   });
 
-  it('DELETE /ACCOUNT_CONFIG/:ID should delete an Account Configuration ', () => {
-    return agent.delete('/account_config/'.concat(accountConfig.id))
-      .then((res) => {
+  it('DELETE /payroll/account_config/:id should delete an Account Configuration ', () => {
+    return agent.delete(`/payroll/account_config/${accountConfig.id}`)
+      .then(res => {
         helpers.api.deleted(res);
       })
       .catch(helpers.handler);
