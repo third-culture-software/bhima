6c6506860e577912e58346b5d7243b7d0f4b0275
Jonathan Niles <jonathanwniles@gmail.com>
2024-12-21 08:48:21 -0600

    refactor(payroll): replace "date_embauche" with"hiring_date"
    Replaces the `date_embauche` with `hiring_date` for better English
    consistency.
    
    Closes #7821.

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -8,3 +8,10 @@
 * invoicing on the patient invoice page.
 */
 ALTER TABLE debtor_group CHANGE max_credit max_debt MEDIUMINT(8) UNSIGNED DEFAULT 0;
+
+/*
+ * @author: jniles
+ * @date: 2024-12-24
+ * Change the name of the "hiring_date" column to "date_embauche" in the "employee" table
+ */
+ALTER TABLE `employee` CHANGE COLUMN date_embauche hiring_date DATETIME DEFAULT NULL;
