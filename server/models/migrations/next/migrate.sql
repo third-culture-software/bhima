91f08ee1cb966ee0eb606058063774c5ffd00fc3
Jonathan Niles <jonathanwniles@gmail.com>
2024-12-21 08:48:21 -0600

    refactor(payroll): replace "date_embauche" with"hiring_date"
    Replaces the `date_embauche` with `hiring_date` for better English
    consistency.
    
    Closes #7821.

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -1,1 +1,8 @@
 -- next migration file
+
+/*
+ * @author: jniles
+ * @date: 2024-12-24
+ * Change the name of the "hiring_date" column to "date_embauche" in the "employee" table
+ */
+ALTER TABLE `employee` CHANGE COLUMN date_embauche hiring_date DATETIME DEFAULT NULL;
