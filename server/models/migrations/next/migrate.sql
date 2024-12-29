67c06fe5d32d5d5d85177d33810521eb885da0f6
Jonathan Niles <jonathanwniles@gmail.com>
2024-12-28 21:05:40 -0600

    chore(HR): correct HR spelling in dictionary.
    Corrects the spelling of "Human Resources" throughout the application.

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -6,3 +6,10 @@
  * Change the name of the "hiring_date" column to "date_embauche" in the "employee" table
  */
 ALTER TABLE `employee` CHANGE COLUMN date_embauche hiring_date DATETIME DEFAULT NULL;
+
+/*
+* @author: jniles
+* @date: 2024-12-28 
+* Correct the spelling of the human resources
+*/
+UPDATE unit SET `path` = 'TREE.HUMAN_RESOURCES' WHERE `path` = 'TREE.HUMANS_RESSOURCES';
