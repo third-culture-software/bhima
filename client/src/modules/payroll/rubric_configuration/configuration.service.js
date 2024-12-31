328abb89c6ae8108514ebc0e521869ce8635f0b4
Jonathan Niles <jonathanwniles@gmail.com>
2024-12-31 12:01:43 -0600

    chore(payroll): Unify the interfaces for rubric configuration
    Uses a unified interface for the rubric configuration service.

--- client/src/modules/payroll/rubric_configuration/configuration.service.js
+++ client/src/modules/payroll/rubric_configuration/configuration.service.js
@@ -1,38 +1,15 @@
 angular.module('bhima.services')
   .service('ConfigurationService', ConfigurationService);
 
-ConfigurationService.$inject = ['PrototypeApiService', '$http', 'util'];
+ConfigurationService.$inject = ['PrototypeApiService'];
 
 /**
  * @class ConfigurationService
  * @extends PrototypeApiService
  *
  * @description
- * Encapsulates common requests to the /rubric_config/ URL.
+ * Encapsulates common requests to the payroll /rubric_config/ URL.
  */
-function ConfigurationService(Api, $http, util) {
-  const service = new Api('/rubric_config/');
-
-  service.getRubrics = getRubrics;
-  service.setRubrics = setRubrics;
-
-  // loads the configuration's rubrics
-  function getRubrics(id) {
-    if (angular.isUndefined(id)) {
-      throw new Error(
-        'Trying to get configuration of rubrics without the identity property',
-      );
-    }
-
-    return $http.get(`/rubric_config/${id}/setting`)
-      .then(util.unwrapHttpResponse);
-  }
-
-  // Sets Payroll Rubric's Configuration using the public API
-  function setRubrics(id, data) {
-    return $http.post(`/rubric_config/${id}/setting`, { configuration : data })
-      .then(util.unwrapHttpResponse);
-  }
-
-  return service;
+function ConfigurationService(Api) {
+  return new Api('/payroll/rubric_config/');
 }
