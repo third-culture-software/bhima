d98dfb356e36b9f425aac6f76c27db661b95d511
Jonathan Niles <jonathanwniles@gmail.com>
2025-01-01 18:37:29 -0600

    chore(accounts): remove tech debt
    Removes technical debt by removing duplicate code.

--- client/src/modules/accounts/accountTypes.service.js
+++ client/src/modules/accounts/accountTypes.service.js
@@ -12,29 +12,15 @@ function AccountTypeService(Api, $http, util) {
   const baseUrl = '/accounts/types/';
   const service = new Api(baseUrl);
 
-  service.getAccountType = getAccountType;
   service.getTypeText = getTypeText;
-
-  /**
-  * @helper
-  * This Method return a list of Account Type
-  * */
-  function getAccountType(id, params) {
-    const url = baseUrl.concat(id || '');
-    return $http.get(url, { params })
-      .then(util.unwrapHttpResponse);
-  }
 
   /**
   * @helper
   * This Method return a text an Account Type
   * */
   function getTypeText(typeId, accountTypes) {
-    const accountText = accountTypes.filter((item) => {
-      return item.id === typeId;
-    });
-
-    return accountText[0].type;
+    const needle = accountTypes.find(item => item.id === typeId);
+    return needle.type;
   }
 
   return service;
