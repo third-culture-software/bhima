9f0bcad0af9089ff479b37b11765e8510db6d282
Jonathan Niles <jonathanwniles@gmail.com>
2025-01-09 12:09:29 -0600

    fix(rubrics): do not crash on empty rubric items
    Does not crash when the rubric items are empty.

--- test/end-to-end/rubricsConfig/rubrics_config.page.js
+++ test/end-to-end/rubricsConfig/rubrics_config.page.js
@@ -18,8 +18,7 @@ class RubricConfigPage {
 
   async create(rubric) {
     await TU.buttons.create();
-    await TU.input('ConfigModalCtrl.rubric.label', rubric.label);
-
+    await TU.input('RubricConfigModalCtrl.config.label', rubric.label);
     await TU.modal.submit();
     await notification.hasSuccess();
   }
@@ -27,7 +26,7 @@ class RubricConfigPage {
   async errorOnCreateRubricConfig() {
     await TU.buttons.create();
     await TU.modal.submit();
-    await TU.validation.error('ConfigModalCtrl.rubric.label');
+    await TU.validation.error('RubricConfigModalCtrl.config.label');
     await TU.modal.cancel();
   }
 
@@ -36,7 +35,7 @@ class RubricConfigPage {
     await row.dropdown();
     await row.edit();
 
-    await TU.input('ConfigModalCtrl.rubric.label', updateRubricConfig.label);
+    await TU.input('RubricConfigModalCtrl.config.label', updateRubricConfig.label);
 
     await TU.modal.submit();
     await notification.hasSuccess();
