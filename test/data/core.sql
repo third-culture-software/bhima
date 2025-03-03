28b58f94b0d73be775659211a25745c62c4b10f7
Jonathan Niles <jonathanwniles@gmail.com>
2025-03-03 17:29:37 -0600

    fix: stock tests fiscal year issue
    For some reason, the fiscal year breakage only catches up to us two
    months after the end of the fiscal year in the stock tests.  This adds
    in the new fiscal year to those stock tests.

--- test/data/core.sql
+++ test/data/core.sql
@@ -112,6 +112,9 @@ CALL CreateFiscalYear(1, @fiscalYear2022, @superUser, 'Fiscal Year 2023', 12, DA
 SET @fiscalYear2024 = 0;
 CALL CreateFiscalYear(1, @fiscalYear2023, @superUser, 'Fiscal Year 2024', 12, DATE('2024-01-01'), DATE('2024-12-31'), 'Notes for 2024', @fiscalYear2024);
 
+SET @fiscalYear2025 = 0;
+CALL CreateFiscalYear(1, @fiscalYear2025, @superUser, 'Fiscal Year 2025', 12, DATE('2025-01-01'), DATE('2025-12-31'), 'Notes for 2025', @fiscalYear2025);
+
 --
 -- Project permission
 --
