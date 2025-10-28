4439f79a37dc3ac1bd2bf2237e5b91d8bb322da7
Jonathan Niles <jonathanwniles@gmail.com>
2025-10-28 16:35:34 -0500

    chore: remove unused path

--- test/server-unit/install.spec.js
+++ test/server-unit/install.spec.js
@@ -1,6 +1,5 @@
 /* eslint global-require:off */
 const { expect } = require('chai');
-const path = require('path');
 
 describe('test/server-unit/install', () => {
 
@@ -20,8 +19,4 @@ describe('test/server-unit/install', () => {
   it('should export proceedInstall function', () => {
     expect(install.proceedInstall).to.be.a('function');
   });
-
-  // Additional integration test would verify that setupDatabaseSchema
-  // can properly read and execute SQL files from server/models/
-  // This requires a database connection and is better suited for integration tests
 });
