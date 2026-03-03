340948c6727ee68ad03f12428c9d6bc6a2628310
Jonathan Niles <jonathanwniles@gmail.com>
2026-03-03 10:19:38 +0100

    fix(tests): ensure that FY 2026 is added
    Adds the FY 2026 to the test harnass.

--- sh/build-stock-database.sh
+++ sh/build-stock-database.sh
@@ -9,7 +9,7 @@ echo "[build]"
 echo "Building BHIMA Stock Database"
 
 set -a
-source .env || { echo '[build-database.sh] did not load .env, using variables from environment.' ; }
+source .env || { echo '[build-database.sh] did not load .env, using variables from environment.'; }
 set +a
 
 # set build timeout
@@ -18,7 +18,7 @@ TIMEOUT=${BUILD_TIMEOUT:-8}
 fout=/dev/null
 
 if [ "$1" == "debug" ]; then
-    fout=/dev/tty
+  fout=/dev/tty
 fi
 
 # default database port
@@ -27,45 +27,102 @@ DB_PORT=${DB_PORT:-3306}
 echo "Building database: $DB_USER,$DB_PASS,$DB_HOST,$DB_PORT,$DB_NAME'"
 
 # build the test database
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" -e "DROP DATABASE IF EXISTS $DB_NAME ;" || { echo 'failed to drop DB' ; exit 1; }
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" -e "CREATE DATABASE $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" || { echo 'failed to create DB' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" -e "DROP DATABASE IF EXISTS $DB_NAME ;" || {
+  echo 'failed to drop DB'
+  exit 1
+}
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" -e "CREATE DATABASE $DB_NAME CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" || {
+  echo 'failed to create DB'
+  exit 1
+}
 
 echo "[build] database schema"
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < server/models/01-schema.sql || { echo 'failed to build DB schema' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <server/models/01-schema.sql || {
+  echo 'failed to build DB schema'
+  exit 1
+}
 
 echo "[build] functions"
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < server/models/02-functions.sql || { echo 'failed to import functions into DB' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <server/models/02-functions.sql || {
+  echo 'failed to import functions into DB'
+  exit 1
+}
 
 echo "[build] procedures"
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < server/models/03-procedures.sql || { echo 'failed to import procedures into DB 1/2' ; exit 1; }
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < server/models/98-admin.sql || { echo 'failed to import procedures into DB 2/2' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <server/models/03-procedures.sql || {
+  echo 'failed to import procedures into DB 1/2'
+  exit 1
+}
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <server/models/98-admin.sql || {
+  echo 'failed to import procedures into DB 2/2'
+  exit 1
+}
 
 echo "[build] triggers"
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < server/models/04-triggers.sql || { echo 'failed to import triggers into DB' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <server/models/04-triggers.sql || {
+  echo 'failed to import triggers into DB'
+  exit 1
+}
 
 echo "[build] default data"
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < server/models/05-icd10.sql || { echo 'failed to import default data into DB 1/2' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <server/models/05-icd10.sql || {
+  echo 'failed to import default data into DB 1/2'
+  exit 1
+}
 
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < server/models/06-bhima.sql || { echo 'failed to import default data into DB 2/2' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <server/models/06-bhima.sql || {
+  echo 'failed to import default data into DB 2/2'
+  exit 1
+}
 
 echo "[build] stock test data"
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < test/data/enterprise.sql || { echo 'failed to import test data into DB 1/5' ; exit 1; }
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < test/data/accounts.sql || { echo 'failed to import test data into DB 2/5' ; exit 1; }
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < test/data/inventories.sql || { echo 'failed to import test data into DB 3/5' ; exit 1; }
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < test/data/core.sql || { echo 'failed to import test data into DB 4/5' ; exit 1; }
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" < test/data/service-stock.sql || { echo 'failed to import test data into DB 5/5' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <test/data/enterprise.sql || {
+  echo 'failed to import test data into DB 1/5'
+  exit 1
+}
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <test/data/accounts.sql || {
+  echo 'failed to import test data into DB 2/5'
+  exit 1
+}
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <test/data/inventories.sql || {
+  echo 'failed to import test data into DB 3/5'
+  exit 1
+}
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <test/data/core.sql || {
+  echo 'failed to import test data into DB 4/5'
+  exit 1
+}
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" <test/data/service-stock.sql || {
+  echo 'failed to import test data into DB 5/5'
+  exit 1
+}
 
 echo "[build] recomputing mappings"
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" -e "Call zRecomputeEntityMap();" || { echo 'failed to recompute mappings 1/2' ; exit 1; }
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" -e "Call zRecomputeDocumentMap();" || { echo 'failed to recompute mappings 2/2' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" -e "Call zRecomputeEntityMap();" || {
+  echo 'failed to recompute mappings 1/2'
+  exit 1
+}
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" -e "Call zRecomputeDocumentMap();" || {
+  echo 'failed to recompute mappings 2/2'
+  exit 1
+}
 
 echo "[build] recalculating period totals"
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" -e "Call zRecalculatePeriodTotals();" || { echo 'failed to recalculate period totals' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" -e "Call zRecalculatePeriodTotals();" || {
+  echo 'failed to recalculate period totals'
+  exit 1
+}
 
 echo "[build] recalulcating stock_movement_status"
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" -e "CALL zRecomputeStockMovementStatus();" || { echo 'failed to recalculate stock movement status' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" -e "CALL zRecomputeStockMovementStatus();" || {
+  echo 'failed to recalculate stock movement status'
+  exit 1
+}
 
 echo "[build] recalulcating stock_value"
-mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" -e "CALL RecomputeStockValue(NULL);" || { echo 'failed to recalculate stock value' ; exit 1; }
+mysql -u "$DB_USER" -p"$DB_PASS" -h"$DB_HOST" -P"$DB_PORT" "$DB_NAME" -e "CALL RecomputeStockValue(NULL);" || {
+  echo 'failed to recalculate stock value'
+  exit 1
+}
 
 echo "[/build]"
