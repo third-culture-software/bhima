516c9035805dc99b71dbfd85ce8e3a41f626b020
lomamech <lomamech@gmail.com>
2026-09-10 08:45:05 +0100

    Clean and sanitaze code

--- server/controllers/payroll/multiplePayroll/commitmentFunction.js
+++ server/controllers/payroll/multiplePayroll/commitmentFunction.js
@@ -63,7 +63,9 @@ function dataCommitment(employees, rubrics, exchangeRates, identificationCommitm
     // FIXME(@jniles) - this gets executed for every employee, even though it is not an employee-specific
     // transaction.  It's linked to the payment period.  It should be instead moved to somewhere that
     // deals with the payment periods, not the employees.
-    const paymentStatus = employee.balance === 0 ? 6 : 3; // 6 = ZERO_BALANCE, 3 = WAITING_FOR_PAYMENT
+    // 6 = ZERO_BALANCE, 3 = WAITING_FOR_PAYMENT
+
+    const paymentStatus = employee.balance === 0 ? 6 : 3;
     transactions.push({
       query : 'UPDATE payment SET status_id = ? WHERE uuid = ?',
       params : [paymentStatus, paymentUuid],
